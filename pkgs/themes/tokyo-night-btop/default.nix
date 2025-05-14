0b4d926f38d391c85bf40245548360191d80fe16
Brennan Bates <brennandb98@gmail.com>
2025-05-14 12:18:32 -0600

    fix btop hash

--- pkgs/themes/tokyo-night-btop/default.nix
+++ pkgs/themes/tokyo-night-btop/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
         owner = "aristocratos";
         repo = "btop";
         rev = version;
-        hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        hash = "sha256-4x2vGmH2dfHZHG+zj2KGsL/pRNIZ8K*sXYRHy0io5IE=";
     };
 
     installPhase = ''
