bf2a7cbeb0db838982305e4f9bd16b62022b63a4
Brennan Bates <brennandb98@gmail.com>
2025-05-14 09:18:46 -0600

    tokyonight btop theme:w

---
+++ pkgs/themes/tokyo-night-btop/default.nix
@@ -0,0 +1,32 @@
+{ lib,
+  stdenv,
+  fetchFromGitHub
+}:
+
+stdenv.mkDerivation rec {
+    pname = "tokyo-night-btop";
+    version = "v4.11.0";
+
+    src = fetchFromGitHub {
+        owner = "folke";
+        repo = "tokyonight.nvim";
+        rev = version;
+        hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp -r * $out
+      runHook postInstall
+    '';
+
+    meta = with lib; {
+        description = "A clean theme that celebrates the lights of Downtown Tokyo at night.";
+        homepage = "https://github.com/folke/tokyonight.nvim";
+        license = liscenses.apache;
+        maintainers = with maintainers; [];
+        mainProgram = "btop";
+        platforms = platforms.all;
+    };
+}
